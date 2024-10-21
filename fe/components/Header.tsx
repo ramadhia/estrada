f60fdcade23d70baf6277dafe0b6bb8530844f5e
ramadhia <rafli.ramadhia@gmail.com>
2024-10-21 17:58:26 +0700

    update header app

--- fe/components/Header.tsx
+++ fe/components/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Header: React.FC = () => {
     return (
         <header className="bg-blue-600 text-white py-4">
-            <h1 className="text-center text-2xl">App Header</h1>
+            <h1 className="text-center text-2xl">Estrada Test</h1>
         </header>
     );
 };
